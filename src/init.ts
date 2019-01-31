ba69e1569e28ecfcf89a6e76401bf960be8f5f76
Sean <sean@pop-os.localdomain>
2019-01-31 11:04:03 +0100

    add schema to package-defs in init

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,9 @@
 import { loadPackageDefs, writePackageDefs } from './package-defs'
+import { join, relative } from 'path'
 
 export function init() {
-  const packageDefs = loadPackageDefs()
+  const packageDefs: any = loadPackageDefs()
+  const schemaFullPath = join(__dirname, 'package-defs.json')
+  packageDefs.$schema = relative(process.cwd(), schemaFullPath)
   writePackageDefs(packageDefs)
 }
