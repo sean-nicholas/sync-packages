4bb0a9b7053b94f438a0e3f380498c37d45e1d9d
Sean <sean@pop-os.localdomain>
2019-01-31 11:07:07 +0100

    no defaults on init

--- src/init.ts
+++ src/init.ts
@@ -2,7 +2,7 @@ import { loadPackageDefs, writePackageDefs } from './package-defs'
 import { join, relative } from 'path'
 
 export function init() {
-  const packageDefs: any = loadPackageDefs()
+  const packageDefs: any = loadPackageDefs({ withDefaults: false })
   const schemaFullPath = join(__dirname, 'package-defs.json')
   packageDefs.$schema = relative(process.cwd(), schemaFullPath)
   writePackageDefs(packageDefs)
